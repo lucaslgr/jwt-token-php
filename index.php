f3a49b9dad4f15b34fc1f2a84b370272259cbe7d
Lucas Guimaraes <lucaslgr1206@gmail.com>
2020-03-30 13:14:15 -0300

    Adicionado as referências

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <?php
     require 'create-test.php';
+    //Referência para entender codificação de base 64 https://www.youtube.com/watch?v=L-cXtP24vAw
+    //Referênica para criptografia HS256 https://www.youtube.com/watch?v=oxmIB5MIJ2c
